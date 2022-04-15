76c01943db7e375ea77c07e03c142f36029ba9ea
khalil el kadih <khalil94elkadih@gmail.com>
2022-04-15 14:44:14 +0000

    AutoLoaded file

--- contact.php
+++ contact.php
@@ -1,6 +1,5 @@
 <?php 
 include_once('db/app.php');
-include_once("code/authentification_code.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,7 +52,7 @@ include_once("code/authentification_code.php");
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </main>
 </body>
