d633cd190f3f7868b4adad43ce73e52b7bf12991
khalil el kadih <khalil94elkadih@gmail.com>
2022-04-18 01:06:30 +0000

    updat contact

---
+++ updateContact.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once('db/app.php');
+include_once('./controller/controllContact.php');
+
+
+if(isset($_POST['updatedata']))
+{   
+
+    $id_contact=validateInput($db->cnx,$_POST['id_contact']);
+
+    $dataInput=
+    [
+        'flname'=>validateInput($db->cnx,$_POST['flname']),
+        'phone'=>validateInput($db->cnx,$_POST['phone']),
+        'email'=>validateInput($db->cnx,$_POST['email']),
+        'adress'=>validateInput($db->cnx,$_POST['adress']),
+    ];
+    $contact = new controllContact;
+    $res=$contact->updatecontact($dataInput,$id_contact);
+    if($res){
+
+        redirect('Contact Updated Successfully','contact.php');
+    }
+    else 
+    {
+        redirect('Something Wrong','profil.php');
+    }
+
+
+}
+
+
+
+
+
+?>
