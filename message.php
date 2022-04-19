740df64476b1bb3e9fd4733d6a4f61bc868005d2
khalil el kadih <khalil94elkadih@gmail.com>
2022-04-19 22:00:35 +0000

    customise function message

--- message.php
+++ message.php
@@ -1,9 +1,19 @@
 <?php
 
-if(isset($_SESSION['message']))
+if(isset($_SESSION['message']) && isset($_SESSION['Typemessage']))
 {
-echo "<h5>".$_SESSION['message']."</h5>";
-unset($_SESSION['message']);
+    if($_SESSION['Typemessage']==="success" )
+    {
+
+        echo '<div class="text-center alert alert-success ">'.$_SESSION['message'].'</div>';
+    unset($_SESSION['message']);
+    }
+    else{
+        echo '<div class="text-center alert alert-danger ">'.$_SESSION['message'].'</div>';
+        unset($_SESSION['message']);
+    }
+
+
 
 }
 
