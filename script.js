1bd377c78f04219b50671bf8e44dc1c5d3396d59
khalil el kadih <khalil94elkadih@gmail.com>
2022-04-19 22:00:07 +0000

    addes some js code form validation form

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
-$(document).ready(function() {
 
+
+
+    // document.forms[0].onsubmit = function(e) {
+    //     e.preventDefault();
+    //     // window.open("../dashbord/Dashbord.html");
+    //     window.location.href = "../dashbord/dashboard.php";
+    // }
+
+
+    const form = document.getElementById('form');
+    const firstName=document.getElementById('firstName');
+    const LastName=document.getElementById('lastName');
+    const email=document.getElementById('email');
+    const password=document.getElementById('password');
+    const password_conf=document.getElementById('conf_pwd');
+    console.log(form);
+
+    function showerror(input ,message)
+    {
+
+        const formControll=input.parentElement;
+        formControll.className='form-control error';
+        const small=formControll.querySelector('small');
+        small.innerText=message;
+    }
+    function showSuccess(input){
+        const formControl=input.parentElement;
+        formControl.className='form-control success';
+    }
+    function checkPassworMatch(input1 ,input2){
+        // console.log("input 1");
+        // console.log(input1);
+        // console.log("input 2");
+        // console.log(input2);
+        
+        const isPassValid=input1.value ==input2.value;
+        if(!isPassValid){
+            showerror(input2,'password not Match ');
+        }
+        return isPassValid;
+        
+    }
+  
+    const checkIfFormIsNotEmpty=(form)=>{
+        let allValide=true;
+        form.querySelectorAll('input').forEach((input)=>{
+            if(input.value==''){
+                showerror(input,input.name+' is required');
+                allValide=false;
+            }else{
+                showSuccess(input);
+            }
+        });
+        return allValide;
+    }
+
+    form.addEventListener('submit',function(e)
+    {
+            if(!checkIfFormIsNotEmpty(e.target) || !checkPassworMatch(password,password_conf))
+            e.preventDefault();
     
-    $('.editbtn').on('click', function(e) {
-      //  console.log(e.target);
-        $('#editmodal').modal('show');
-
-        $tr = $(this).closest('tr');
-        // console.log($tr[0]);
-
-        let data = $tr.children("td").map(function() {
-            console.log(this);
-            return $(this).text();
-        }).get();
-        
-        // console.log(data);
-        
-        /////////////////////////
-
-        let children=$tr.children();
-        let id_contact=children[0].textContent;
-         let name=children[1].textContent;
-         let email=children[2].textContent;
-        let phone=children[3].textContent;
-         let adress=children[4].textContent;
-        $('#editmodal .id_contact').val(data[0]);
-        $('#editmodal .flname').val(data[1]);
-        $('#editmodal .email').val(data[2]);
-        $('#editmodal .phone').val(data[3]);
-        $('#editmodal .adress').val(data[4]);
-       
-    });
-
-
-
-
-
-              
-
-});
+    })
